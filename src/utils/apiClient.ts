2f7169f0dba3f6206241d7722acea83b84060394
Sameer725 <samkhanal725@gmail.com>
2022-05-23 18:20:29 +0545

    feat: add react router

---
+++ src/utils/apiClient.ts
@@ -0,0 +1,22 @@
+const apiURL = process.env.REACT_APP_API_URL
+
+export const client = async (
+  endpoint: string,
+  params: Record<string, string> = {},
+  method: RequestInit['method'] = 'GET',
+) => {
+  let url = `${apiURL}/${endpoint}`
+
+  if (Object.keys(params).length > 0) {
+    url += '?' + new URLSearchParams(params).toString()
+  }
+
+  return window.fetch(url, {method}).then(async response => {
+    const data = await response.json()
+    if (response.ok) {
+      return data
+    } else {
+      return Promise.reject(data)
+    }
+  })
+}
